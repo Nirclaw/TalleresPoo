fd53d851d6701328235ce29e816641c1fb53f50f
Nirclaw <caicedonicolas150@gmail.com>
2023-05-25 16:46:05 -0500

    feat: :sparkles: se agregar finalizado el taller 3 basico
    Se finaliza el taller 3 finalizado con su js y su html

--- tallerBasico/taller3/main.js
+++ tallerBasico/taller3/main.js
@@ -22,8 +22,6 @@ class Figura {
   }
 }
 
-
-
 class Circulo extends Figura {
   radio;
   constructor({ radio, color, area }) {
@@ -36,42 +34,88 @@ class Circulo extends Figura {
   get radio() {
     return this.radio;
   }
-  calcularArea(radio){
-    return Math.PI * radio
+  calcularArea(radio) {
+    return Math.PI * radio;
+  }
+}
+
+class Rectangulor extends Figura {
+  largo;
+  ancho;
+  constructor({ largo, ancho, color }) {
+    super({ color });
+    this.ancho = ancho;
+    this.largo = largo;
+  }
+  get largo() {
+    return this.largo;
+  }
+  set nuevoLargo(nuevoLargo) {
+    this.largo = nuevoLargo;
+  }
+  get ancho() {
+    return this.ancho;
+  }
+  set nuevoAncho(nuevoAncho) {
+    this.ancho = nuevoAncho;
+  }
+  calcularArea(largo, ancho) {
+    return largo * ancho;
   }
 }
 
 
-class Rectangulor extends Figura{
-    largo
-    ancho
-    constructor({largo,ancho,color}){
-        super({color})
-        this.ancho=ancho
-        this.largo=largo
-    }
-    get largo(){
-        return this.largo
-    }
-    set nuevoLargo(nuevoLargo){
-        this.largo=nuevoLargo
-    }
-    get ancho(){
-        return this.ancho
-    }
-    set nuevoAncho(nuevoAncho){
-        this.ancho=nuevoAncho
-    }
-    calcularArea(largo,ancho){
-        return largo*ancho
-    }
-}
+const formulario = document.querySelector("form");
+const botonA = document.getElementById("btn");
 
-let figura1 = new Figura({ color: "rojo", area: 23 });
+botonA.addEventListener("click", (el) => {
+  el.preventDefault();
+  let datos = Object.fromEntries(new FormData(formulario));
+  console.log(datos);
+  switch (datos.figura) {
+    case "Circulo":
+      let ciruculo1 = new Circulo({ radio: datos.radio, color: datos.color });
+      return (document.getElementById(
+        "cartas"
+      ).innerHTML += ` <div class="col-3">
+   <div class="card text-bg-success m-3" style="max-width: 18rem;">
+   <div class="card-header">tarjeta</div>
+   <div class="card-body">
+   <h3 class="card-title">2023</h3>
+   <p class="card-text">
+   <ul>
+   <li><b>${ciruculo1.calcularArea(ciruculo1.radio)}</b></li>
+   <li>${ciruculo1.color}</li>
+   <li>${datos.figura}</li>
+   </ul>
+   </p>
+   </div>` ,
+   formulario.reset());
+      
+    case "Rectangulo":
+      let rectangulo1 = new Rectangulor({
+        largo: datos.largo,
+        ancho: datos.ancho,
+        color: datos.color,
+      });
+      return (document.getElementById(
+        "cartas"
+      ).innerHTML += ` <div class="col-3">
+   <div class="card text-bg-success m-3" style="max-width: 18rem;">
+   <div class="card-header">tarjeta</div>
+   <div class="card-body">
+   <h3 class="card-title">2023</h3>
+   <p class="card-text">
+   <ul>
+   <li><b>${rectangulo1.calcularArea(rectangulo1.largo,rectangulo1.ancho)}</b></li>
+   <li>${rectangulo1.color}</li>
+   <li>${datos.figura}</li>
+   </ul>
+   </p>
+   </div>`,formulario.reset());
+   
+  }
+  /*HTML*/
 
-console.log(figura1.calcularArea(figura1.area));
-let ciruculo1 = new Circulo({radio:10,color:'verde',area:10})
-
-console.log(ciruculo1.calcularArea(ciruculo1.radio));
-let rectangulo1=new Rectangulor({largo:10,ancho:20,color:'azul'})
-console.log(rectangulo1.calcularArea(rectangulo1.largo,rectangulo1.largo));
+  
+});
